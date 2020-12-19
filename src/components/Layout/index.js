f711298ead5c44a9c867f4c2be1d32025df13d82
= <jimmy010679@gmail.com>
2020-12-19 14:44:41 +0800

    12/19 test

---
+++ src/components/Layout/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+//import styles from "./layout.module.css"
+
+const Layout = ({ children, type }) => {
+  return <React.Fragment>{children}</React.Fragment>
+}
+
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
+import { Button } from "@material-ui/core"
+import Layout from "../components/Layout"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Home = () => {
+  return (
+    <Layout>
+      Hello world!<Button color="primary">Hello World</Button>
+    </Layout>
+  )
 }
+
+export default Home
