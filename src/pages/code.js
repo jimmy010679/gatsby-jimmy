59257ea743594c6d38a944d838bd4a5ee14dc17b
= <jimmy010679@gmail.com>
2021-01-02 03:08:00 +0800

    add code highlight component

--- src/pages/code.js
+++ src/pages/code.js
@@ -1,10 +1,8 @@
 import React from "react"
 
-import SEO from "../components/Common/SEO"
 import Layout from "../components/Layout"
-
-import SyntaxHighlighter from "react-syntax-highlighter"
-import { a11yDark } from "react-syntax-highlighter/dist/esm/styles/hljs"
+import SEO from "../components/Common/SEO"
+import { PostCode } from "../components/Common/PostCode"
 
 import { Container } from "@material-ui/core"
 
@@ -20,27 +18,6 @@ const Code = () => {
     'status'=>'ok',
      'COPDBRTH'=> $jsonData['COPDBRTH']
   );
-
-
-  function curl_post($url,$post) {
-
-  	$payload = json_encode($post);
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_POST,true);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST,'POST');
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-    curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-    $result = curl_exec($ch);
-    curl_close ($ch);
-    return $result;
-  }
-
-  $postWEB = curl_post('http://10.1.1.10:8000/api/test', $jsonData);
-  #$postWEB = curl_post('http://10.1.1.10:8000/api/test', $arrData);
-  echo $postWEB;
   `
 
   return (
@@ -48,9 +25,7 @@ const Code = () => {
       <SEO />
       <div className="blogNews">
         <Container maxWidth="lg">
-          <SyntaxHighlighter language="php" style={a11yDark}>
-            {codeString}
-          </SyntaxHighlighter>
+          <PostCode language="php" code={codeString} />
         </Container>
       </div>
     </Layout>
