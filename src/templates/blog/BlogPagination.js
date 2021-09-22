d3ba84e79c93ec871bcd85eba5e95e0418cef552
= <jimmy010679@gmail.com>
2021-09-23 00:47:15 +0800

    add: BlogPagination

---
+++ src/templates/blog/BlogPagination.js
@@ -0,0 +1,88 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+import { getImage, GatsbyImage } from "gatsby-plugin-image"
+
+import Layout from "../../components/Layout"
+
+import Container from "@mui/material/Container"
+
+const BlogPagination = ({ pageContext, location, data }) => {
+  const { currentPage, numPages } = pageContext
+
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numPages
+  const prevPage =
+    currentPage - 1 === 1 ? "/blog/" : (currentPage - 1).toString()
+  const nextPage = (currentPage + 1).toString()
+
+  // ------------------------------------------------------------------------------------------------
+  // return
+  return (
+    <Layout path={location.pathname}>
+      <Container maxWidth="md">
+        <h1>{currentPage}</h1>
+
+        <div>
+          {data?.allMarkdownRemark?.nodes.map((article, index) => (
+            <div key={article.frontmatter.id}>
+              <Link to={`/blog/article/${article.frontmatter.id}/`}>
+                <GatsbyImage
+                  image={getImage(article.frontmatter.cover)}
+                  alt="aaa"
+                />
+              </Link>
+              <Link to={`/blog/article/${article.frontmatter.id}/`}>
+                {article.frontmatter.title}
+              </Link>
+            </div>
+          ))}
+        </div>
+        <div>
+          {!isFirst && (
+            <Link to={prevPage} rel="prev">
+              ← Previous Page
+            </Link>
+          )}
+          {!isLast && (
+            <Link to={nextPage} rel="next">
+              Next Page →
+            </Link>
+          )}
+        </div>
+      </Container>
+    </Layout>
+  )
+}
+
+export default BlogPagination
+
+export const queryArticles = graphql`
+  query blogListQuery($skip: Int!, $limit: Int!) {
+    allMarkdownRemark(
+      filter: {
+        fileAbsolutePath: { regex: "/content/blog/" }
+        frontmatter: { published: { eq: true } }
+      }
+      limit: $limit
+      skip: $skip
+      sort: { order: DESC, fields: frontmatter___id }
+    ) {
+      nodes {
+        frontmatter {
+          id
+          title
+          cover {
+            childImageSharp {
+              gatsbyImageData(
+                placeholder: BLURRED
+                formats: [AUTO, WEBP]
+                width: 800
+                aspectRatio: 1.77
+              )
+            }
+          }
+        }
+      }
+    }
+  }
+`
